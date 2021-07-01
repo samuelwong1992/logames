4cd74c9b6b7b7c21dad68bb4447c7e062ba380cf
Samuel Wong <samuelwong1992@gmail.com>
2021-07-01 10:54:46 +1000

    Initial Commit

---
+++ Logames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logames
+//
+//  Created by Samuel Wong on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
